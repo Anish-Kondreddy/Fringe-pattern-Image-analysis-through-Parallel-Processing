c9463dfe27a0889aa6e8d0c6e38c9181260d423f
Anish-Kondreddy <129581700+Anish-Kondreddy@users.noreply.github.com>
2025-08-25 16:06:43 +0100

    Update ColourTOB&W.py
    Source code

--- ColourTOB&W.py
+++ ColourTOB&W.py
@@ -1,1 +1,15 @@
-
+# import opencv 
+import cv2  # type: ignore
+  
+# Load the input image 
+image = cv2.imread('C:\\Users\\info\\Desktop\\Coding\\Pattern.png') 
+cv2.waitKey(0) 
+  
+# Use the cvtColor() function to grayscale the image 
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+  
+cv2.imwrite('Grayscale.png', gray_image)
+cv2.waitKey(0)   
+  
+# Window shown waits for any key pressing event 
+cv2.destroyAllWindows()
